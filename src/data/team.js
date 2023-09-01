5fc23a2c7b38ef1c2e8e2a0bf29c5ae9b9686021
frontdevuz <davlatovizzatillo7@gmail.com>
2023-09-01 05:55:46 +0500

    add new page

---
+++ src/data/team.js
@@ -0,0 +1,7 @@
+export const Type = [
+    { id: 1, name: "Frontend Developer", },
+    { id: 2, name: "Backend Developer", },
+    { id: 3, name: "Full stack Developer", },
+    { id: 4, name: "Graphic Designer", },
+    { id: 5, name: "Flutter developer", },
+    { id: 6, name: "Tester", }]
--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Menu from "../../components/menu/menu";
 import Team from "../.././assets/images/team.jpg";
 import Aboutimg from "../.././assets/images/Company.jpg";
 import Container from "../../components/container/container";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { Type } from "./../../data/team";
 import "./about.scss";
 export default function About() {
+  useEffect(() => {
+    Aos.init();
+  });
   return (
     <React.Fragment>
       <Menu>About us</Menu>
@@ -13,41 +19,65 @@ export default function About() {
           <div className="about__container">
             <header className="about__header">
               <div className="about__img">
-                <img src={Aboutimg} alt="This is a about company img" />
+                <img
+                  src={Aboutimg}
+                  alt="This is a about company img"
+                  data-aos="zoom-in "
+                  data-aos-duration="7000"
+                  data-aos-easing="linear"
+                  data-aos-delay="300"
+                />
               </div>
-              <div className="about__text">
+              <div
+                className="about__text"
+                data-aos="zoom-in"
+                data-aos-duration="4000"
+                data-aos-easing="linear"
+                data-aos-delay="450"
+              >
                 <h2 className="about__text__title">About of Company</h2>
                 <p className="about__text__paragraph">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptatibus ad, quaerat temporibus dolores eaque repellat
-                  maxime ullam dolor, cupiditate sed, tenetur accusantium
-                  cumque? Corporis maiores voluptate placeat molestias, corrupti
-                  perferendis, sit dolorem alias voluptas maxime ad. Ratione
-                  sapiente officia recusandae consequatur ducimus, eligendi eos
-                  debitis vero aliquid similique doloremque. Perspiciatis
-                  molestias sequi eaque dolorem repellendus distinctio enim quam
-                  sint quis maxime! Id nemo at eius ullam ad culpa illo
-                  possimus.
+                  Voluptatibus voluptas maxime ad. Ratione sapiente officia
+                  recusandae consequatur ducimus, eligendi eos debitis vero
+                  aliquid similique doloremque. Perspiciatis molestias sequi
+                  eaque dolorem repellendus distinctio enim quam sint quis
+                  maxime! Id nemo at eius ullam ad culpa illo possimusam
+                  doloremque! Praesentium animi et, saepe reprehenderit repellat
+                  nam, rem cupiditate debitis mollitia doloremque odit harum
+                  nisi optio! Facere doloribus accusantium, dicta nostrum natus
+                  ipsam! Officiis nobis, voluptatum quod qui provident earum,
+                  numquam atque temporibus iusto reiciendis distinctio ea vitae
+                  nisi fugiat eaque nihil ipsum.
                 </p>
               </div>
             </header>
             <section className="about__team">
               <div className="about__img">
-                <img src={Team} alt="This is a about img" />
+                <img
+                  src={Team}
+                  alt="This is a about img"
+                  data-aos="zoom-in "
+                  data-aos-duration="7000"
+                  data-aos-easing="linear"
+                  data-aos-delay="300"
+                />
               </div>
               <div className="about__text">
                 <p className="about__text__title">About Team</p>
-                <p className="about__text__paragraph">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                  Iusto esse vel soluta, inventore repudiandae quidem id,
-                  provident voluptates ad, illo asperiores sed! Aperiam expedita
-                  itaque laudantium consequatur aspernatur harum esse assumenda
-                  ad voluptate at, repudiandae quis repellendus provident libero
-                  totam qui iste vero consequuntur a molestiae. Ullam odio harum
-                  atque magnam, illum rem, quos officiis recusandae impedit vel
-                  reiciendis temporibus soluta, distinctio quae pariatur iure
-                  libero expedita. Ea, facere velit.
-                </p>
+                <ul>
+                  {Type.map((item, index) => (
+                    <li
+                      className="about__text__li"
+                      data-aos="zoom-in"
+                      data-aos-duration="7000"
+                      data-aos-easing="linear"
+                      data-aos-delay="450"
+                    >
+                      {item.name}
+                    </li>
+                  ))}
+                </ul>
               </div>
             </section>
           </div>
